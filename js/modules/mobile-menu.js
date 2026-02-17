9d1b8da82369d5d8a4c0c6b5ce7cfde0b4db0d22
Diego <diegolimabarenco@gmail.com>
2026-02-16 21:26:34 -0300

    Delete js directory

--- js/modules/mobile-menu.js
+++
@@ -1,6 +0,0 @@
-export default function initMobileMenu() {
-    // Logic could be expanded here if we add a hamburger button
-    // For now, this is a placeholder or simples expansion 
-    // depending on if the user wants a full hamburger menu implementation
-    // We can start by adding a simple class toggle if we inject a button
-}
